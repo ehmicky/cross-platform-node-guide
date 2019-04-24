aaa3eac9fd2ef3129c387f2a898518ee4bc5252d
ehmicky <ehmicky@gmail.com>
2019-04-24 10:00:00 +0200

    Change eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+// We use a JavaScript file instead of YAML so that this repository gets
+// categorized as JavaScript by GitHub.
+'use strict'
+
+module.exports = {
+  extends: 'standard-prettier-fp/.eslintrc.yml'
+}
