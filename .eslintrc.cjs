9ff0aa205e78ab45a86edc72a8e291ae381ffd31
ehmicky <ehmicky@gmail.com>
2024-10-27 14:13:59 +0000

    Fix linting

--- .eslintrc.cjs
+++
@@ -1,6 +0,0 @@
-// We use a JavaScript file instead of YAML so that this repository gets
-// categorized as JavaScript by GitHub.
-// eslint-disable-next-line filenames/match-regex
-module.exports = {
-  extends: '@ehmicky/eslint-config/.eslintrc.yml',
-}
